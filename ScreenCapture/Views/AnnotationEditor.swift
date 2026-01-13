2efe2c8536924bb6c0c7cac05b8e34acaf21cc8d
Matthew Sweeney <mail@mcs.dev>
2026-01-13 21:38:43 +0000

    Update ScreenCapture/Views/AnnotationEditor.swift

--- ScreenCapture/Views/AnnotationEditor.swift
+++ ScreenCapture/Views/AnnotationEditor.swift
@@ -766,6 +766,9 @@ struct ColorPickerGrid: View {
     @Binding var selectedColor: Color
     @Binding var showPicker: Bool
 
+    // Local state to buffer ColorPicker updates and prevent crash from rapid binding updates
+    @State private var pickerColor: Color = .red
+
     private let colors = Color.annotationColors
 
     var body: some View {
@@ -785,11 +788,17 @@ struct ColorPickerGrid: View {
 
             DSDivider()
 
-            ColorPicker("Custom", selection: $selectedColor)
+            ColorPicker("Custom", selection: $pickerColor)
                 .labelsHidden()
+                .onChange(of: pickerColor) { _, newColor in
+                    selectedColor = newColor
+                }
         }
         .padding(DSSpacing.md)
         .background(Color.dsBackgroundElevated)
+        .onAppear {
+            pickerColor = selectedColor
+        }
     }
 }
 
